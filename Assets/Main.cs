b7d6910deb9090d57ccf14563864c9655fe6725f
Pellanor <pellanor@gmail.com>
2018-12-01 00:35:19 -0700

    Simple UI working

---
+++ Assets/Main.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour {
+
+    public Text questText;
+
+	void Start () {
+	}
+	
+	void Update () {
+		
+	}
+
+    public void SwipeRight() {
+        questText.text = "You swiped right!";
+    }
+
+    public void SwipeLeft()
+    {
+        questText.text = "You swiped left!";
+    }
+}
